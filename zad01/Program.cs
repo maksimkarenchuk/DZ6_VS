acab7b697e79c8a2dd07ebd8b15bdc5936cb50dd
max <matrixbyine.gmail.com>
2022-09-07 22:12:34 +0300

    Точка пересечения прямых

--- zad01/Program.cs
+++ zad01/Program.cs
@@ -1,23 +1,25 @@
-﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+﻿// Напишите программу, которая найдёт точку пересечения двух прямых, 
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
 
-// 0, 7, 8, -2, -2 -> 2
-// 1, -7, 567, 89, 223-> 3
 
 
-void PozitivNumbers()
-{
-      Console.WriteLine("Сколько чисел Вы хотите ввести: ");
-      int M = Convert.ToInt32(Console.ReadLine());
-      int count = 0;
-      int countNum = 0;
-            while (countNum < M)
-            {
-                Console.Write("Введите число: "); 
-                int x = Convert.ToInt32(Console.ReadLine());
-                if (x > 0) count += 1;
-                countNum += 1;
-            }
-      Console.WriteLine($"Положительных чисел: {count}");
-}
+void PointX()
+        {
+            Console.WriteLine("Введите k1 ");
+            int k1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите b1 ");
+            int b1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите k2 ");
+            int k2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите b2 ");
+            int b2 = Convert.ToInt32(Console.ReadLine());
+            int x;
+            int y;
 
-PozitivNumbers();
+                x = (b2-b1)/(k1-k2);
+                y = k1 * x + b1;
+
+        Console.WriteLine($"Точка пересечения прямых ({x};{y})");
+        }
+Point();
